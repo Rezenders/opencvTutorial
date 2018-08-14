6cc28cceed42816f5711578cd65fa80f1c7fbed4
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2018-08-14 13:53:37 -0300

    ➕ substituindo .hpps para apenas opencv.hpp

--- rgb2gray.cpp
+++ rgb2gray.cpp
@@ -1,11 +1,8 @@
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+#include <opencv2/opencv.hpp>
 
 int main(){
 	cv::Mat pokan = cv::imread("imgs/pokan.png");
 	cv::Mat gray_pokan;
 	cvtColor(pokan, gray_pokan, cv::COLOR_BGR2GRAY);
-	cv::imwrite("imgs/gray_pokan.png", gray_pokan);	
-
+	cv::imwrite("imgs/gray_pokan.png", gray_pokan);
 }
